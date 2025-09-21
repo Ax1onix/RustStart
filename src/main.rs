c35ea96ee60ba25a87f7c0af81b898ab4ec6f7d1
Axionix <yanio.tut@gmail.com>
2025-09-21 15:15:04 +0300

    added skull emoji XD

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let count: u32 = 1;
+    let skull = '\u{1F480}';
     println!("Hello, world!");
-    println!("This project is being made by {} person!", count)
+    println!("This project is being made only by {} person {}", count, skull)
 }
