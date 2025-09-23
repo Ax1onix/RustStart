e27756d083936dda07a6ed05dba8c22a0816d725
Axionix <yanio.tut@gmail.com>
2025-09-23 21:57:26 +0300

    added decision

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-fn main() {
+use std::io;
+
+fn main() -> io::Result<()> {
     let count: u32 = 1;
     let skull = '\u{1F480}';
     println!("Hello, world!");
-    println!("This project is being made only by {} person {}", count, skull)
+    println!(
+        "This project is being made only by {} person {}",
+        count, skull
+    );
+    println!("How many people are working on the project?????");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    if input == "1" {
+        loop {
+            println!("GOOD JOB!!!");
+        }
+    } else {
+        loop {
+            println!("{}{}WRONG!!!!{}{}", skull, skull, skull, skull);
+        }
+    }
 }
